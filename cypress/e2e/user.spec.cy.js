9d7a299f2cd8ffe8516d089592325d2ba65b08bd
suelen <suelen.im@outlook.com>
2024-02-14 22:00:45 -0300

    User update scenario added

---
+++ cypress/e2e/user.spec.cy.js
@@ -0,0 +1,49 @@
+import userData from '../fixtures/userData.json'
+
+describe('Orang HMR tests', () => {
+
+  const selectorList = {
+    usernameField: "[name='username']",
+    passwordField: "[name='password']",
+    loginButton: "[type='submit']",
+    selectonTetleTopBar: ".oxd-topbar-header-breadcrumb > .oxd-text",
+    dashboardGrid: ".orangehrm-dashboard-grid",
+    wrongCredentialAlert: "[role='alert']",
+    myInfoButton: '[href="/web/index.php/pim/viewMyDetails"]',
+    firstNameField: "[name='firstName']",
+    lastNameField: "[name='lastName']",
+    genericField: ".oxd-input--active",
+    dateField: "[placeholder='yyyy-dd-mm']",
+    dateCloseButton: ".--close",
+    submitButton: "[type='submit']"
+  }
+
+  it.only('User Info Update - Success', () => {
+    cy.visit('/auth/login')
+    cy.get(selectorList.usernameField).type(userData.userSuccess.username)
+    cy.get(selectorList.passwordField).type(userData.userSuccess.password)
+    cy.get(selectorList.loginButton).click()
+    cy.location('pathname').should('equal', '/web/index.php/dashboard/index')
+    cy.get(selectorList.dashboardGrid)
+    cy.get(selectorList.myInfoButton).click()
+    cy.get(selectorList.firstNameField).clear().type('firstNameTest')
+    cy.get(selectorList.lastNameField).clear().type('lastNameTest')
+    cy.get(selectorList.genericField).eq(3).clear().type('Test')
+    cy.get(selectorList.genericField).eq(4).clear().type('OtherIdTest')
+    cy.get(selectorList.genericField).eq(5).clear().type('123456')
+    cy.get(selectorList.dateField).eq(0).clear().type('2025-03-10')
+    cy.get(selectorList.dateCloseButton).click()
+    cy.get(selectorList.submitButton).eq(0).click()
+    cy.get('body').should('contain', 'Successfully Updated')
+    cy.get('.oxd-toast-close')
+    
+    //cy.get(selectorList.genericField).eq(6).clear().type('DriversTest')
+  })
+  it('Login - Fail', () => {
+    cy.visit('/auth/login')
+    cy.get(selectorList.usernameField).type(userData.userFail.username)
+    cy.get(selectorList.passwordField).type(userData.userFail.password)
+    cy.get(selectorList.loginButton).click()
+    cy.get(selectorList.wrongCredentialAlert)
+  })
+})
